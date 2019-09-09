91cd0c7c39eef751e745768893c405a4e7d3b245
snanilim <snanilim@gmail.com>
2019-09-09 17:36:51 +0600

    basic communication with tkinter and mqtt done

--- oopLogin.py
+++ oopLogin.py
@@ -9,28 +9,62 @@ import re, uuid
 import time
 
 
-uuid.uuid4 # generating randorm number in base64
-r = str(uuid.uuid4())
-client = mqtt.Client(r)
-broker = "broker.hivemq.com"
-port = 1883
 
-def on_connect(client, userdata, flags, rc):
-	print("Connected with result code " + str(rc))
+class MQTTClient(object):
+    def __init__(self, *args, **kwargs):
+        try:
+            uuid.uuid4
+            r = str(uuid.uuid4())
+            
+            self.result = bool
+            self.msg = dict
 
-def on_message(client, userdata, message):
-    print("topic: "+message.topic+"	"+"payload: "+str(message.payload.decode("utf-8")))
-    check_topic(str(message.payload.decode("utf-8")))
+            self.BROKER_IP = "broker.hivemq.com"
+            self.PORT = 1883
 
+            self.client = mqtt.Client(r)
+            self.client.on_connect = self.on_connect
+            self.client.on_message = self.on_message
+            self.client.connect(self.BROKER_IP, self.PORT, 60)
 
+            print(f"Connected to {0}, starting MQTT loop")
+            # self.client.loop_forever()
+            # self.client.loop_start()
+        except Exception as e:
+            print("error", e)
 
-client.on_connect = on_connect  #attach the callback function to the client object 
-client.on_message = on_message	#attach the callback function to the client object
+    def on_message(self,client,userdata,message):
+        print("topic: "+message.topic+"	"+"payload: "+str(message.payload.decode("utf-8")))
+        msg = str(message.payload.decode("utf-8"))
+        data = json.loads(msg)
+        value = data.get("result", "")
+        self.result = eval(value)
+        self.on_loop_stop()
 
-def check_topic(msg):
-    print('msg', msg)
-    client.loop_stop()
-    client.disconnect()
+    def on_connect(self, client, userdata, flags, rc):
+        print("Connected with result code " + str(rc))
+        self.client.subscribe("srdl/res_login/", 1)
+
+    def on_publish(self, topicUri, message):
+        topic = message
+        topic_dump = json.dumps(topic)
+        self.client.publish(topicUri, topic_dump)
+        print('Publish')
+
+    def on_loop_forever(self):
+        print('work')
+        self.client.loop_forever()
+
+    def on_loop_stop(self):
+        print('work')
+        self.client.loop_stop()
+        self.client.disconnect()
+
+        
+
+# client = MQTTClient()
+        
+
 
 class Container(tk.Tk):
     def __init__(self, *args, **kwargs):
@@ -52,7 +86,7 @@ class Container(tk.Tk):
 
         self.frames = {}
 
-        for F in (LoginPage, PageOne):
+        for F in (LoginPage, LabIDPage, InfoPage, ErrorPage):
             print(F)
             frame = F(container, self, FrameBack)
             self.frames[F] = frame
@@ -76,11 +110,7 @@ class LoginPage(tk.Frame):
     def __init__(self, parent, controller, FrameBack):
         tk.Frame.__init__(self, parent, bg=FrameBack)
 
-        client.connect(broker,port,60)
-        # client.loop_start()
         
-
-        client.subscribe("srdl/res_login/", 1)
         
         label = tk.Label(self, bg=FrameBack, font=('Courier', 22), text="Login", justify='left')
         label.place(relwidth=1, relheight=0.2)
@@ -107,21 +137,97 @@ class LoginPage(tk.Frame):
 
             
             # mqtt start
-            
-            client.publish("srdl/login/", "Hello world!")
-            client.loop_forever()
-            
-            
-
+            client = MQTTClient()
+            info = {"userid" : userid, "password": password}
+            client.on_publish('srdl/login/', info)
+            client.on_loop_forever()
+            result = client.result
             # mqtt end
 
-            controller.show_frame(PageOne)
+            if result:
+                print('result', result)
+                controller.show_frame(LabIDPage)
+            else:
+                controller.show_frame(ErrorPage)
             
 
-class PageOne(tk.Frame):
+class LabIDPage(tk.Frame):
     def __init__(self, parent, controller, FrameBack):
         tk.Frame.__init__(self, parent, bg=FrameBack)
-        label = tk.Label(self, text="Inner Page")
+
+        label = tk.Label(self, bg=FrameBack, font=('Courier', 22), text="Lab ID", justify='left')
+        label.place(relwidth=1, relheight=0.2)
+
+        labIDLabel = tk.Label(self, bg=FrameBack, font=('Courier', 15), text="LabID:", justify='left')
+        labIDLabel.place(relx=0.03, rely=0.25, relwidth=0.3, relheight=0.12, anchor='nw')
+
+        labIDEntry = tk.Entry(self, font=('Courier', 15))
+        labIDEntry.place(relx=0.30, rely=0.25, relwidth=0.6, relheight=0.12)
+
+
+        button = tk.Button(self, text="Submit", font=('Courier', 12), bg="#749492", command=lambda: get_info(labIDEntry.get(), controller))
+        button.place(relx=0.35, rely=0.70, relheight=0.15, relwidth=0.4)
+
+        def get_info(labid, controller):
+            print(f'lab ID {labid}')
+
+
+            
+            # mqtt start
+            client = MQTTClient()
+            info = {"labid" : labid}
+            client.on_publish('srdl/labid/', info)
+            client.on_loop_forever()
+            result = client.result
+            # mqtt end
+
+            if result:
+                print('result', result)
+                controller.show_frame(InfoPage)
+            else:
+                controller.show_frame(ErrorPage)
+
+
+class InfoPage(tk.Frame):
+    def __init__(self, parent, controller, FrameBack):
+        tk.Frame.__init__(self, parent, bg=FrameBack)
+
+        label = tk.Label(self, bg=FrameBack, font=('Courier', 22), text="Info Page", justify='left')
+        label.place(relwidth=1, relheight=0.2)
+
+        labIDLabel = tk.Label(self, bg=FrameBack, font=('Courier', 15), text="CPU:", justify='left')
+        labIDLabel.place(relx=0.03, rely=0.25, relwidth=0.3, relheight=0.12, anchor='nw')
+
+
+
+        button = tk.Button(self, text="Submit", font=('Courier', 12), bg="#749492", command=lambda: get_info("info", controller))
+        button.place(relx=0.35, rely=0.70, relheight=0.15, relwidth=0.4)
+
+        def get_info(info, controller):
+            print(f'lab ID {info}')
+
+            # mqtt start
+            client = MQTTClient()
+            info = {"info" : info}
+            client.on_publish('srdl/labid/', info)
+            client.on_loop_forever()
+            result = client.result
+            # mqtt end
+
+            if result:
+                print('result', result)
+                # controller.show_frame(LabIDPage)
+                app.quit()
+                print('quit')
+                
+            else:
+                controller.show_frame(ErrorPage)
+
+
+class ErrorPage(tk.Frame):
+    def __init__(self, parent, controller, FrameBack):
+        tk.Frame.__init__(self, parent, bg=FrameBack)
+        label = tk.Label(self, text="Something went wrong. Please try again", fg="red")
         label.pack(pady=10, padx=10)
 
         button1 = tk.Button(self, text="Back", command=lambda: controller.show_frame(LoginPage))
@@ -132,6 +238,8 @@ class PageOne(tk.Frame):
 
 
 app = Container()
-# app.check_topic('aaa')
 app.mainloop()
+app.destroy()
+client = MQTTClient()
+client.on_loop_forever()
 
